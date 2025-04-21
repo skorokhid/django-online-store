da6f7bea9eb9704ebe0c2214546e9ca8ad9b56fe
Arthur Skorokhid <arthur.skorokhid@gmail.com>
2025-04-21 11:17:30 +0300

    Add GitHub Actions workflow

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get(reverse('home'))  # або '/' якщо не використовуєш name в url
+        self.assertEqual(response.status_code, 200)
